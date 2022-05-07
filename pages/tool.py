358aca99d6866a1ada827cfbebe56250138410d9
Alex <alexander.s.carney.ug@dartmouth.edu>
2022-05-07 18:48:49 -0400

    Added jeremy...

--- pages/tool.py
+++ pages/tool.py
@@ -50,33 +50,3 @@ def app():
     register_callback("my_key", click_image_callback)
 
     col1.file_uploader("Upload Images", type=["png", "jpg", "jpeg"], key="uploaded_img", on_change=upload_image_callback)
-
-    # register_callback("my_key", click_image_callback)
-
-    # if image_file is not None:
-    #
-    #     print("I AM IN SIDE OF HERE RIGHT NOW")
-    #
-    #
-    #     image = Image.open(image_file)
-    #
-    #     # To See details
-    #     file_details = {"filename": image_file.name, "filetype": image_file.type,
-    #               "filesize": image_file.size}
-    #     # st.write(file_details)
-    #
-    #     # To View Uploaded Image
-    #     fig = px.imshow(image)
-    #     st.session_state.clicked_points = plotly_events(fig, key="uploaded_image")
-    #
-    #     converted_img = np.array(image.convert('RGB'))
-    #
-    #     register_callback("uploaded_image", click_image_callback)
-
-
-
-
-        # print(converted_img)
-        # print(np.shape(converted_img))
-        #
-        # st.image(converted_img, channels='GRB')
