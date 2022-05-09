28317f47966a1cd1550a5c0d63278740fc21c7b6
Alex <alexander.s.carney.ug@dartmouth.edu>
2022-05-09 17:45:32 -0400

    Removed unneccessary prints and comments. Ready for production

--- common/convolution.py
+++ common/convolution.py
@@ -1,38 +1,7 @@
 import numpy as np
 from PIL import Image
 
-def load_image(image_file):
-    img = Image.open(image_file)
-    return img
 
-myImage = Image.open("jeremy.jpg");
-myImage.show();
-
-
-image = load_image("jeremy.jpg")
-
-
-print(image.size)
-
-converted_img = np.array(image.convert('RGB'))
-print(converted_img)
-
-image_row = np.ndim(converted_img)
-print(image_row)
-
-image_size = np.size(converted_img)
-print(image_size)
-
-imgGray = image.convert('L')
-imgGray.save('test_gray.jpg')
-
-b_w_img = np.array(imgGray)
-print(b_w_img)
-
-print(np.size(imgGray))
-print(np.ndim(imgGray))
-
-print(range(1,np.size(imgGray)[0]-2))
 
 
 def convolution(image, kernel):
@@ -47,14 +16,69 @@ def convolution(image, kernel):
     kernel_dim = int(np.sqrt(np.size(kernel)))
     img_row = np.shape(image)[0]
     img_col = np.shape(image)[1]
+    img_colors = np.shape(image)[2]
 
     image_array = np.array(image)
 
-    convoluted_img = np.zeros((img_row, img_col))
+    convoluted_img = np.array(image)
+    #convoluted_img = np.array(image)
 
     ker_idx_max = kernel_dim - 2
     for row in range(ker_idx_max, img_row - ker_idx_max):
         for col in range(ker_idx_max, img_col - ker_idx_max):
+            result = np.array([0])
+            """
+            The kernel has the following representation (assuming 3x3):
+            [(-1, -1) (-1, 0) (-1, 1)]
+            [(0,  -1) (0,  0) (0,  1)]
+            [(1,  -1) (1,  0) (1,  1)]
+            Therefore, our parameter of interest is kernel_dim - 2
+            """
+            for ker_row in range(-ker_idx_max, ker_idx_max + 1):
+                for ker_col in range(-ker_idx_max, ker_idx_max + 1):
+                    # print(str(row - ker_row) + " + " + str(col - ker_col))
+                    result = result + (kernel[ker_row + ker_idx_max, ker_col + ker_idx_max]
+                               * image_array[(row - ker_row), (col - ker_col)])
+            convoluted_img[row, col] = result
+
+    final_image = Image.fromarray(convoluted_img)
+    return final_image
+
+
+def convolution_2(image, kernel, starting_points, range_around):
+    """
+    Performs convolution on a specified section with a given kernel
+    @author Jane O'Reilly
+    :param image: A PIL image object
+    :param kernel: A square matrix with odd dimension
+    :param starting_point: the starting pixel value, must be within the dimensions of the image and be a 1x2 array
+    :param range_around: the area that will be convolved
+    :return: convolved image
+    """
+
+    kernel_dim = int(np.sqrt(np.size(kernel)))
+    img_row = np.shape(image)[0]
+    img_col = np.shape(image)[1]
+
+    image_array = np.array(image)
+
+    # convoluted_img = np.zeros((img_row, img_col))
+
+    ker_idx_max = 1 if kernel_dim == 3 else 2
+
+    convoluted_img = np.array(image)
+
+    # starting and ending row
+    start_row = starting_points[0] - range_around if (starting_points[0] - range_around) >= 0 else 0
+    end_row = starting_points[0] + range_around if (starting_points[0] + range_around) <= img_row - 1 else (img_row-1)
+
+    # starting and ending column
+    start_col = starting_points[1] - range_around if (starting_points[1] - range_around) >= 0 else 0
+    end_col = starting_points[1] + range_around if (starting_points[1] + range_around) <= img_col - 1 else (img_col-1)
+
+
+    for row in range(start_row, end_row):
+        for col in range(start_col, end_col):
             result = 0
             """
             The kernel has the following representation (assuming 3x3):
@@ -66,24 +90,66 @@ def convolution(image, kernel):
             for ker_row in range(-ker_idx_max, ker_idx_max + 1):
                 for ker_col in range(-ker_idx_max, ker_idx_max + 1):
                     # print(str(row - ker_row) + " + " + str(col - ker_col))
-                    result += kernel[ker_row + ker_idx_max, ker_col + ker_idx_max] * image_array[(row - ker_row), (col - ker_col)]
+                    result += (kernel[ker_row + ker_idx_max, ker_col + ker_idx_max] * image_array[
+                        (row - ker_row), (col - ker_col)]).astype(np.int16)
             convoluted_img[row, col] = result
 
     final_image = Image.fromarray(convoluted_img)
     return final_image
 
 
-kernal=np.zeros((3,3))
-print(np.size(kernal))
-print(np.sqrt(np.size(kernal)))
+def main():
 
-convolution(imgGray,kernal).show()
+    def load_image(image_file):
+        img = Image.open(image_file)
+        return img
 
-kernal_gauss = np.array([[1/16, 2/16, 1/16], [2/16, 4/16, 2/16], [1/16, 2/16, 1/16]])
-convolution(imgGray,kernal_gauss).show()
+    myImage = Image.open("jeremy.jpg");
+    myImage.show();
 
-kernal_box = np.array([[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]])
-convolution(imgGray,kernal_box).show()
 
-kernal_sharp = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-convolution(imgGray,kernal_sharp).show()
+    image = load_image("jeremy.jpg")
+
+
+    print(image.size)
+
+    converted_img = np.array(image.convert('RGB'))
+    print(converted_img)
+
+    image_row = np.ndim(converted_img)
+    print(image_row)
+
+    image_size = np.size(converted_img)
+    print(image_size)
+
+    imgGray = image.convert('L')
+    imgGray.save('test_gray.jpg')
+
+    b_w_img = np.array(imgGray)
+    print(b_w_img)
+
+    print(np.size(imgGray))
+    print(np.ndim(imgGray))
+
+    print(range(1,np.size(imgGray)[0]-2))
+
+
+    kernal=np.zeros((3,3))
+    print(np.size(kernal))
+    print(np.sqrt(np.size(kernal)))
+
+    # convolution(imgGray,kernal).show()
+
+    kernal_gauss = np.array([[1/16, 2/16, 1/16], [2/16, 4/16, 2/16], [1/16, 2/16, 1/16]])
+    # convolution(converted_img, kernal_gauss).show()
+    # convolution(imgGray,kernal_gauss).show()
+    #
+    # kernal_box = np.array([[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]])
+    # convolution(imgGray,kernal_box).show()
+    #
+    convolution(converted_img, kernal_gauss).show()
+
+
+if __name__ == '__main__':
+    main()
+
--- pages/tool.py
+++ pages/tool.py
@@ -4,36 +4,59 @@ import numpy as np
 import plotly.express as px
 from streamlit_plotly_events import plotly_events
 from common.components_callbacks import register_callback
+import common.convolution as conv
+import json
+
+kernels = {
+    "Gauss Blur 3x3": np.array([[1 / 16, 2 / 16, 1 / 16], [2 / 16, 4 / 16, 2 / 16], [1 / 16, 2 / 16, 1 / 16]]),
+    "Gauss Blur 5x5": (1/256) * np.array([[1, 4, 6, 4, 1], [4, 16, 24, 16, 4], [6, 24, 36, 24, 6], [4, 16, 24, 16, 4], [1, 4, 6, 4, 1]]),
+    "Box Blur": (1/9) * np.ones((3, 3)),
+    "Bruh": np.array([[0, 1, 0], [1, 5, 1], [0, 1, 0]]),
+    "????": np.random.rand(3, 3)
+}
+
+
 
 def app():
-    def load_image(image_file):
-        img = Image.open(image_file)
-        return img
 
     def upload_image_callback():
         print(st.session_state.uploaded_img)
         if st.session_state.uploaded_img is not None:
-            print("INSIDE UPLOAD IMAGE")
             image = Image.open(st.session_state.uploaded_img)
             render_image(image)
 
     def render_image(image):
-        print("RENDERING IMAGE")
         st.session_state.curr_image = image
         fig = px.imshow(image)
         if st.session_state.just_uploaded:
             st.session_state.just_uploaded = False
-            with col2:
-                plotly_events(fig, key="my_key")
+            plotly_events(fig, key="my_key")
+
+    def option_change_callback():
+        if 'curr_image' in st.session_state:
+            render_image(st.session_state.curr_image)
 
 
     def click_image_callback():
-        print("INSIDE CLICK IMAGE CALLBACK")
-        print("You clicked " + str(st.session_state.my_key))
 
         # Get the current image. Do something to it, then pass it on to render_image
+        str_without_brack = st.session_state.my_key[1:-1]
+        result = json.loads(str_without_brack)
+        print(result)
+        st.session_state.curr_image = conv.convolution_2(st.session_state.curr_image,
+                                                         kernels[st.session_state.kernel],
+                                                         result['pointIndex'],
+                                                         st.session_state.blur_radius)
 
         render_image(st.session_state.curr_image)
+
+    def blur_full_image_callback():
+        try:
+            st.session_state.curr_image = conv.convolution(st.session_state.curr_image,
+                                                           kernels[st.session_state.kernel])
+            render_image(st.session_state.curr_image)
+        except:
+            st.warning("Can't do this right now. Upload an image first")
 
 
 # --------------------------------- WIDGETS
@@ -49,4 +72,12 @@ def app():
 
     register_callback("my_key", click_image_callback)
 
-    col1.file_uploader("Upload Images", type=["png", "jpg", "jpeg"], key="uploaded_img", on_change=upload_image_callback)
+    st.file_uploader("Upload Images", type=["png", "jpg", "jpeg"], key="uploaded_img", on_change=upload_image_callback)
+
+    col1.button("Reset Image", key="reset", on_click=upload_image_callback)
+
+    col1.button("Full Image", on_click=blur_full_image_callback)
+
+    col2.slider("Blur Radius", key="blur_radius", min_value=5, max_value=500, value=50, step=1, on_change=option_change_callback)
+
+    col2.selectbox("Kernel", ["Gauss Blur 3x3", "Gauss Blur 5x5", "Box Blur", "Bruh", "????"], key='kernel', on_change=option_change_callback)
